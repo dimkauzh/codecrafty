0c7f4cc614e05726dbfb48322818aa4c6f08556e
Dima Marhitych <uzhdimka@gmail.com>
2023-08-08 18:06:57 +0200

    Changed folder structure, added menu.cpp and changed the engine to a header

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,23 @@
-#include "import.hpp"
-#include "engine.cpp"
-#include "fonts.cpp"
+#include "engine/import.hpp"
+#include "menu.cpp"
 
 class codecrafty {
-    public:
-        codecrafty() {
-            init();
+public:
+    codecrafty() {
+        init();
 
-        }
-    private:
-        engine engine;
-        fonts all_fonts;
+    }
+private:
+    char title[11] = "codecrafty";
+    int width = 1280;
+    int height = 720;
 
-        char title[11] = "codecrafty";
-        int width = 1280;
-        int height = 720;
+    void init() {
+        InitWindow(width, height, title);
+        SetTargetFPS(60);
 
-        void init() {
-            InitWindow(width, height, title);
-            SetTargetFPS(60);
-
-            Menu();
-        }
-
-        void Menu() {
-
-            Button new_project(engine.new_rect(50, 300, 150, 45), GRAY, LIGHTGRAY, "New Project");
-            Button open_project(engine.new_rect(50, 350, 150, 45), GRAY, LIGHTGRAY, "Open Project");
-
-            while (!WindowShouldClose())
-            {
-
-                BeginDrawing();
-
-                    ClearBackground(RAYWHITE);
-                    DrawText("Welcome to codecrafty!", 400, 10, 30, BLACK);
-                    new_project.draw();
-                    open_project.draw();
-
-
-                EndDrawing();
-
-                if (new_project.is_pressed()) {
-                    engine.print("New Project");
-                }
-                else if (open_project.is_pressed()) {
-                    engine.print("Open Project");
-                }
-
-            }
-        }
+        menu menu;
+    }
 };
 
 int main() {
