cee481c3aad7b8fc00dddf4c4bca975861bbaf39
Dima Marhitych <uzhdimka@gmail.com>
2023-10-01 10:36:32 +0200

    Fixed tabs issue

--- src/menu/menu.go
+++ src/menu/menu.go
@@ -6,7 +6,7 @@ import (
 	bd "codecrafty/src/backend"
 
 	"fyne.io/fyne/v2"
-  "fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/container"
 )
 
 func Menu(w fyne.Window) {
@@ -16,9 +16,9 @@ func Menu(w fyne.Window) {
 	newproject.IsPressed(newProject)
 	openproject.IsPressed(openProject)
 
-  content := container.NewWithoutLayout(newproject.Container(), openproject.Container())
- 
-  bd.DrawContainer(w, content)
+	content := container.NewWithoutLayout(newproject.Container(), openproject.Container())
+
+	bd.DrawContainer(w, content)
 
 }
 
